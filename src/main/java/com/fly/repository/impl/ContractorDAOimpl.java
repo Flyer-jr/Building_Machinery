b32de9a2aba97b52ca6f755c7acdae6e7e784e94
Flyer <Flyer-jr@yandex.com>
2020-02-16 15:45:45 +0300

    Added some DAO repositories

--- src/main/java/com/fly/repository/impl/ContractorDAOimpl.java
+++ src/main/java/com/fly/repository/impl/ContractorDAOimpl.java
@@ -3,12 +3,16 @@ package com.fly.repository.impl;
 import com.fly.repository.dao.ContractorDAO;
 import com.fly.repository.entities.Contractor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class ContractorDAOimpl implements ContractorDAO {
@@ -53,21 +57,53 @@ public class ContractorDAOimpl implements ContractorDAO {
 
     @Override
     public Contractor findById(Long id) {
-        return null;
+
+        final String findQuery = "select * from m_contractor where id = :contractorId";
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("contractorId", id);
+        return namedParameterJdbcTemplate.queryForObject(findQuery,parameterSource, this::getContractorRowMapper);
     }
 
     @Override
     public void delete(Long id) {
+        final String deleteQuery = "DELETE * from m_contractor where id = :contractorId";
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("contractorId", id);
+        namedParameterJdbcTemplate.update(deleteQuery,parameterSource);
 
     }
 
     @Override
     public Contractor save(Contractor entity) {
-        return null;
+
+        final String saveQuery = "INSERT INTO m_contractor (short_name, full_name, adress)" +
+                "VALUES (:shortName, :fullName, :adress)";
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("shortName", entity.getShortName());
+        parameterSource.addValue("fullName", entity.getFullName());
+        parameterSource.addValue("adress", entity.getAdress());
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        namedParameterJdbcTemplate.update(saveQuery, parameterSource,keyHolder);
+
+        Long createdContactorId = Objects.requireNonNull(keyHolder.getKey().longValue());
+
+        return findById(createdContactorId);
     }
 
     @Override
     public Contractor update(Contractor entity) {
-        return null;
+
+        final String updateQuery = "UPDATE m_contractor SET short_name = :shortName, full_name = :fullName" +
+                "adress = :adress where id = :contractorId";
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("shortName", entity.getShortName());
+        parameterSource.addValue("fullName", entity.getFullName());
+        parameterSource.addValue("adress", entity.getAdress());
+        parameterSource.addValue("contractorId",entity.getId());
+
+        namedParameterJdbcTemplate.update(updateQuery,parameterSource);
+        return findById(entity.getId());
     }
 }
